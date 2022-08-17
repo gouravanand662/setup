307276765bc697000402fc4dcea301a8ff4fbcdb
Gourav Anand <gouravanand662@gmail.comgit config --global user.email gouravanand662@gmail.comgit config --global user.email gouravanand662@gmail.com>
2022-08-17 06:27:52 +0530

    Updates

---
+++ precache-manifest.9dd2e29f748111a04c9151020a595278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02658f845a8364f858d195466ae9db20",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "b211b01c6053685887b1",
+    "url": "/slider/static/css/main.f86758df.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/slider/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b211b01c6053685887b1",
+    "url": "/slider/static/js/main.ed9a7537.chunk.js"
+  },
+  {
+    "revision": "ce1caf34cf0aba241fa2",
+    "url": "/slider/static/js/runtime-main.c6fd19b8.js"
+  }
+]);
